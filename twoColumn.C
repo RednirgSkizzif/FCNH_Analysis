c3fcfbd44e00d93504c894e22bd6aa21527efb78
Dylan Frizzell <dylan.frizzell@ou.edu>
2016-08-29 12:30:06 -0500

    "Added the dat files from the 100k signal sample. Also edited the twoColumn script to also provide the integration of the plot."

--- twoColumn.C
+++ twoColumn.C
@@ -15,13 +15,16 @@ file->ls();
 while(1){
 ofstream myfile;
 string output;
-double sum=0;
 
+
+try{
 cout <<" What is the name of the histogram to extract column data from?"<<endl;
 cin.ignore(1000,'\n');
 getline (cin,plot);
 TH1D* h1 = (TH1D*)file->Get(plot.c_str());
 double binWidth = h1->GetXaxis()->GetBinWidth(1);
+
+
 cout <<" What is the name of the text file you want output (example.txt)?"<<endl;
 cin >> output;
 myfile.open(output.c_str());
@@ -33,17 +36,20 @@ double x=0;
 for (int i = 1;i<bins-1;i++){
 x=binWidth+x;
 myfile<<setw(12)<< x-binWidth/2<<"                    " << h1->GetBinContent(i)/binWidth<< endl;
-sum = sum + h1->GetBinContent(i)/binWidth;
 }
 
-myfile <<endl<< "The total Integral of the plot is : " << h1->Integral()<<endl;
-myfile << "The sum method of Integration gives : " << sum <<endl;
-myfile << " bins are " << h1->GetXaxis()->GetBinWidth(1) << " GeV wide "<<endl;
+myfile <<endl<< " bins are " << h1->GetXaxis()->GetBinWidth(1) << " GeV wide "<<endl;
 int entries =  h1->GetEntries();
+myfile << " The Integral is " << h1->Integral() <<endl;
 myfile << " Number of entries = " << entries<< endl;
 myfile << " Number of bins = " << bins-2<<endl;
 myfile << " Note I am cutting off the first and last bins (over and under flow)"<<endl;
 myfile.close();
+
+}catch(ExceptionName e){cout << "Please choose a histogram in the file!"<<endl;
+return 1;
+}
+
 cout << "Would you like to do another histogram from this file (y/n)?"<<endl;
 string again;
 cin >> again;
